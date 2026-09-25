28ac29156847fd4aa6be8940c0947800674c214f
KavaiDariusz <dariuszkavai@icloud.com>
2026-09-25 23:04:07 +0300

    Tweaked one more time the background to run more smoothly, also finished the info about myself, also expanded projects a lil bit, changed logos to text and made it phone compatible

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a github page for my infoweb/cv website. The purpose for the repo is to show my progression, and to be able to work from different computers.
+
+![WebsiteImage](WebsiteImage.png) 
--- background.js
+++ background.js
@@ -1,175 +1,220 @@
 let Mat = [];
+let updating = false;
 
-const Size = 16;
-const cols = Math.floor(window.innerWidth / Size);
-const rows = Math.floor(window.innerHeight / Size);
+const BaseSize = 16;
+let Size = 0;
+let cols;
+let rows;
 
-for(let i = 0; i<rows; i++){
-    Mat[i]=[];
-    for(let j = 0; j<cols; j++)
-        Mat[i][j] = 0;
-}
+let resizeTimeout;
 
 const presets = [
     [
-        [0,0,0],
-        [1,1,1],
-        [0,0,0]
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
     ],
     [
-        [0,0,1],
-        [1,0,1],
-        [0,1,1]
+        [0, 0, 1],
+        [1, 0, 1],
+        [0, 1, 1]
     ],
     [
-        [0,0,1,1,0],
-        [1,1,0,1,1],
-        [1,1,1,1,0],
-        [0,1,1,0,0]
+        [0, 0, 1, 1, 0],
+        [1, 1, 0, 1, 1],
+        [1, 1, 1, 1, 0],
+        [0, 1, 1, 0, 0]
     ],
     [
-        [1,1,0],
-        [1,0,1],
-        [0,1,0]
+        [0, 1, 1, 0, 0],
+        [1, 1, 0, 1, 1],
+        [0, 1, 1, 1, 1],
+        [0, 0, 1, 1, 0]
     ],
     [
-        [0,0,0,0,1,1,0],
-        [1,1,1,1,0,1,1],
-        [1,1,1,1,1,1,0],
-        [0,1,1,1,1,0,0]
+        [1, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0]
     ],
     [
-        [0,0,1,1,1,0,0,0,1,1,1,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,0,0,0,0,1,0,1,0,0,0,0,1],
-        [1,0,0,0,0,1,0,1,0,0,0,0,1],
-        [1,0,0,0,0,1,0,1,0,0,0,0,1],
-        [0,0,1,1,1,0,0,0,1,1,1,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,1,1,1,0,0,0,1,1,1,0,0],
-        [1,0,0,0,0,1,0,1,0,0,0,0,1],
-        [1,0,0,0,0,1,0,1,0,0,0,0,1],
-        [1,0,0,0,0,1,0,1,0,0,0,0,1],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,1,1,1,0,0,0,1,1,1,0,0],
+        [0, 0, 0, 0, 1, 1, 0],
+        [1, 1, 1, 1, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 0, 0]
     ],
     [
-        [1,1,1],
-        [1,0,1],
-        [1,1,1],
-        [1,1,1],
-        [1,1,1],
-        [1,1,1],
-        [1,0,1],
-        [1,1,1],
+        [0, 1, 1, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 1, 1],
+        [0, 1, 1, 1, 1, 1, 1],
+        [0, 0, 1, 1, 1, 1, 0]
     ],
     [
-        [1,1,1],
-        [1,1,1],
-        [1,1,1]
+        [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+    ],
+    [
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+    ],
+    [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
     ]
 ];
 
-let matrixDiv = document.getElementById("matrix");
-//  Tell CSS how many columns the grid has
-matrixDiv.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-matrixDiv.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-
-//  Setting up the background
-for (let i = 0; i < rows; i++) 
-    for (let j = 0; j < cols; j++) {
-        let cell = document.createElement("div");
-
-        cell.classList.add("cell");
-
-        if (Mat[i][j] === 1) 
-            cell.style.backgroundColor = "white";
-        else 
-            cell.style.backgroundColor = "black";
-        
-            matrixDiv.appendChild(cell);
-        }
+const canvas = document.getElementById("matrix");
+const ctx = canvas.getContext("2d");
 
 //k = which preset
-function CheckPreset(k,i,j){
-    for(let ii = 0; ii<presets[k].length; ii++)
-        for(let jj = 0; jj<presets[k][ii].length; jj++)
-            if(i+ii>=rows || j+jj>=cols)
+function CheckPreset(k, i, j) {
+    for (let ii = 0; ii < presets[k].length; ii++)
+        for (let jj = 0; jj < presets[k][ii].length; jj++)
+            if (i + ii >= rows || j + jj >= cols)
                 return 0;
     return 1;
 }
-function DrawPreset(k,i,j){  
-    for(let ii = 0; ii<presets[k].length; ii++)
-        for(let jj = 0; jj<presets[k][ii].length; jj++)
-            Mat[i+ii][j+jj]=presets[k][ii][jj];
+function DrawPreset(k, i, j) {
+    for (let ii = 0; ii < presets[k].length; ii++)
+        for (let jj = 0; jj < presets[k][ii].length; jj++)
+            Mat[i + ii][j + jj] = presets[k][ii][jj];
 }
 
-function UpdateGrid(){
-    let cells = matrixDiv.children;
+function UpdateCanvas() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < rows; i++) 
+    for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            let cell = cells[i*cols + j];
 
-            if (Mat[i][j] === 1) 
-                cell.style.backgroundColor = "white";
-            else 
-                cell.style.backgroundColor = "black";
+            if (Mat[i][j] === 1) {
+                ctx.fillStyle = "white";
+                ctx.fillRect(
+                    j * Size,
+                    i * Size,
+                    Size,
+                    Size
+                );
+            }
         }
+    }
 }
 
-function CalculateNeighbours(i,j){
+function CalculateNeighbours(i, j) {
     let sum = 0;
-    for(let ii = -1; ii<=1; ii++)
-        for(let jj = -1; jj<=1; jj++){
-            if(i+ii< 0 || i+ii>=rows || j+jj<0 || j+jj>=cols || (ii==0 && jj==0))
+    for (let ii = -1; ii <= 1; ii++)
+        for (let jj = -1; jj <= 1; jj++) {
+            if (i + ii < 0 || i + ii >= rows || j + jj < 0 || j + jj >= cols || (ii == 0 && jj == 0))
                 continue;
-            sum+=Mat[i+ii][j+jj];
+            sum += Mat[i + ii][j + jj];
         }
     return sum;
 }
 
-function UpdateLife(){
+function UpdateLife() {
     let Mat2 = [];
 
-    for(let i = 0; i<rows; i++){
-        Mat2[i]=[];
-        for(let j = 0; j<cols; j++)
+    for (let i = 0; i < rows; i++) {
+        Mat2[i] = [];
+        for (let j = 0; j < cols; j++)
             Mat2[i][j] = 0;
     }
 
-    for(let i = 0; i<rows; i++)
-        for(let j = 0; j<cols; j++){
-            let neighbours = CalculateNeighbours(i,j);
-                if(Mat[i][j]==0 && neighbours==3)
-                    Mat2[i][j]=1;
-                else if(Mat[i][j] && (neighbours == 2 || neighbours==3))
-                    Mat2[i][j]=1;
+    for (let i = 0; i < rows; i++)
+        for (let j = 0; j < cols; j++) {
+            let neighbours = CalculateNeighbours(i, j);
+            if (Mat[i][j] == 0 && neighbours == 3)
+                Mat2[i][j] = 1;
+            else if (Mat[i][j] && (neighbours == 2 || neighbours == 3))
+                Mat2[i][j] = 1;
         }
 
-    for(let i = 0; i<rows; i++)
-        for(let j = 0; j<cols; j++)
-            Mat[i][j]=Mat2[i][j];
+    if (!updating)
+        for (let i = 0; i < rows; i++)
+            for (let j = 0; j < cols; j++)
+                Mat[i][j] = Mat2[i][j];
 }
 
-function DrawRandomlyPresets(amount){
-    for(let i = 0; i<amount; i++){
+function DrawRandomlyPresets(amount) {
+    for (let i = 0; i < amount; i++) {
         let ii = Math.floor(Math.random() * rows);
         let jj = Math.floor(Math.random() * cols);
         let k = Math.floor(Math.random() * presets.length);
-        if(CheckPreset(k,ii,jj))
-            DrawPreset(k,ii,jj);
+        if (CheckPreset(k, ii, jj))
+            DrawPreset(k, ii, jj);
         else
             i--;
     }
 }
 
-DrawRandomlyPresets(30);
-
 function update() {
-    UpdateGrid();
+    UpdateCanvas();
     UpdateLife();
 
-    setTimeout(update, 500);
+    setTimeout(update, 1000);
 }
-setTimeout(update, 500);
+
+
+function GenerateBackground() {
+    Mat = [];
+    updating = true;
+
+    let currentSize = BaseSize
+
+    cols = Math.floor(window.innerWidth / currentSize);
+    rows = Math.floor(window.innerHeight / currentSize);
+
+    while (cols * rows < 5000) {
+        currentSize /= 2;
+        cols = Math.floor(window.innerWidth / currentSize);
+        rows = Math.floor(window.innerHeight / currentSize);
+    }
+
+    while (cols * rows > 10000) {
+        currentSize *= 2;
+        cols = Math.floor(window.innerWidth / currentSize);
+        rows = Math.floor(window.innerHeight / currentSize);
+    }
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    Size = currentSize;
+
+    for (let i = 0; i < rows; i++) {
+        Mat[i] = [];
+        for (let j = 0; j < cols; j++)
+            Mat[i][j] = 0;
+    }
+
+    DrawRandomlyPresets(20);
+    updating = false;
+}
+
+window.addEventListener("resize", () => {
+    clearTimeout(resizeTimeout);
+
+    resizeTimeout = setTimeout(() => {
+        GenerateBackground();
+    }, 200);
+});
+
+GenerateBackground();
+setTimeout(update, 1000);
